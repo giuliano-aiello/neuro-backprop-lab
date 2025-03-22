e4a1683ad1aef7d7dc57e4eb247a43b6c9a3a137
giuliano <giuliano.aiello.97@gmail.com>
2025-03-22 18:09:19 +0100

    feat (model): add class

---
+++ model/model_mnist.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+class ModelMNIST(nn.Module):
+
+    MNIST_IMAGE_SIZE = 28 * 28
+    MNIST_NUM_CLASSES = 10
+
+    def __init__(self):
+        super(ModelMNIST, self).__init__()
+        self.layer_input = nn.Flatten()
+        self.layer_fully_connected = nn.Linear(ModelMNIST.MNIST_IMAGE_SIZE, 128)
+        self.layer_output = nn.Linear(128, ModelMNIST.MNIST_NUM_CLASSES)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.layer_input(x)
+        x = self.relu(self.layer_fully_connected(x))
+        x = self.layer_output(x)
+        return x
